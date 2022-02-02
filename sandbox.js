10c4e5bd62837969f3cfbdb653a3b4781504a2de
Ketan Jakhar <70067813+ketan-jakhar@users.noreply.github.com>
2022-02-02 14:39:25 +0530

    compliling typescript

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+var character = "mario";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
