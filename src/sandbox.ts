214af63336332daaca93ea0f7ca8f7d62f2716c6
Ketan Jakhar <70067813+ketan-jakhar@users.noreply.github.com>
2022-02-11 13:51:29 +0530

    Function Signature

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,11 +1,25 @@
-type StrOrNum = string | number;
+// example 1
+let greet: (a: string, b: string) => void;
 
-const logDetails = (uid: StrOrNum, item: string) => {
-	console.log(`${item} has a uid of ${uid}`);
+greet = (name: string, greeting: string) => {
+	console.log(`${name} says ${greeting}`);
 };
 
-type objWithName = { name: string; uid: StrOrNum };
+// example 2
+let calc: (a: number, b: number, c: string) => number;
+calc = (numOne: number, numTwo: number, action: string) => {
+	if (action === "add") {
+		return numOne + numTwo;
+	} else {
+		return numOne - numTwo;
+	}
+};
 
-const greet = (user: objWithName) => {
-	console.log(`${user.name} says hello!`);
+// example 3
+let logDetails: (obj: { name: string; age: number }) => void;
+
+type person = { name: string; age: number };
+
+logDetails = (ninja: person) => {
+	console.log(`${ninja.name} is ${ninja.age} years old`);
 };
